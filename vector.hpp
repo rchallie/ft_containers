189a3145b5860da7c40ca63127fcb8a06aa806fe
BuildTools <unconfigured@null.spigotmc.org>
2020-05-23 22:55:52 +0200

    Add all prototypes to vector

--- vector.hpp
+++ vector.hpp
@@ -6,7 +6,7 @@
 /*   By: excalibur <excalibur@student.42.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/04/18 12:45:54 by excalibur         #+#    #+#             */
-/*   Updated: 2020/05/23 18:29:33 by excalibur        ###   ########.fr       */
+/*   Updated: 2020/05/23 22:53:14 by excalibur        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -88,28 +88,40 @@ namespace ft
         ** That can read or modify any element stored.
         ** Convertible to const_iterator;
         */
+        /** ________________________ WIP ________________________
+         * (Need to be replace by own reverse iterator)
+        */
         typedef std::random_access_iterator_tag             iterator;
 
         /*
         ** A random access iterator to const value_type
         ** That can read element stored.
         */
+        /** ________________________ WIP ________________________
+         * (Need to be replace by own reverse iterator)
+        */
         typedef std::random_access_iterator_tag             const_iterator;
         
         /*
-        ** std::reverse_iterator<iterator>
+        ** ft::reverse_iterator<iterator>
         ** That can read or modify any element in a reversed vector.
         ** Used to iterate through the vector in reverse.
+        */
+        /** ________________________ WIP ________________________
+         * (Need to be replace by own reverse iterator)
         */
         typedef std::reverse_iterator<iterator>             reverse_iterator;
 
         /*
-        ** std::reverse_iterator<const_iterator>
+        ** ft::reverse_iterator<const_iterator>
         ** That can read any element in a reversed the vector.
         ** Can't be used to modify, used to iterate through the
         ** the vector in reverse.
         */
-        typedef std::reverse_iterator<const_iterator>       const_reverse_iterator;
+        /** ________________________ WIP ________________________
+         * (Need to be replace by own reverse iterator)
+        */
+       typedef std::reverse_iterator<const_iterator>       const_reverse_iterator;
 
         /*
         ** A signed integral type:
@@ -137,6 +149,8 @@ namespace ft
 
         public:
 
+            // Constructors:
+            
             /*
             ** @brief Default.
             ** Constructs an empty container, with no elements.
@@ -157,18 +171,19 @@ namespace ft
 
             /*
             ** Fill
+            ** Use insert / assign.
             */
             /** ________________________ WIP ________________________*/
-            // explicit vector (size_type n, const value_type& val = value_type(),
-            //                  const allocator_type& alloc = allocator_type());
+            explicit vector (size_type n, const value_type& val = value_type(),
+                             const allocator_type& alloc = allocator_type());
 
             /*
             ** Range
             */
             /** ________________________ WIP ________________________*/
-            // template <class InputIterator>
-            //         vector (InputIterator first, InputIterator last,
-            //                 const allocator_type& alloc = allocator_type());
+            template <class InputIterator>
+                    vector (InputIterator first, InputIterator last,
+                            const allocator_type& alloc = allocator_type());
             
             /*
             ** Copy
@@ -176,8 +191,37 @@ namespace ft
             /** ________________________ WIP ________________________*/
             vector (const vector&);
             
+            /** ________________________ WIP ________________________*/
             virtual ~vector() {};
+            
+            /** ________________________ WIP ________________________*/
             vector &operator=(const vector& op);
+
+            // Iterators:
+
+            /** ________________________ WIP ________________________*/
+            iterator begin();
+
+            /** ________________________ WIP ________________________*/
+            const_iterator begin() const;
+
+            /** ________________________ WIP ________________________*/
+            iterator end();
+
+            /** ________________________ WIP ________________________*/
+            const_iterator end() const;
+
+            /** ________________________ WIP ________________________*/
+            reverse_iterator rbegin();
+
+            /** ________________________ WIP ________________________*/
+            const_reverse_iterator rbegin() const;
+
+            /** ________________________ WIP ________________________*/
+            reverse_iterator rend();
+
+            /** ________________________ WIP ________________________*/
+            const_reverse_iterator rend() const;
 
             // Capacity:
             
@@ -231,8 +275,111 @@ namespace ft
 
             /** ________________________ WIP ________________________*/
             void        reserve (size_type n) { (void)n; }
+
+            // Element access:
+
+            /** ________________________ WIP ________________________*/
+            reference operator[] (size_type n);
+
+            /** ________________________ WIP ________________________*/
+            const_reference operator[] (size_type n) const;
             
+            /** ________________________ WIP ________________________*/
+            reference at (size_type n);
+
+            /** ________________________ WIP ________________________*/
+            const_reference at (size_type n) const;
+
+            /** ________________________ WIP ________________________*/
+            reference front ();
+
+            /** ________________________ WIP ________________________*/
+            const_reference front () const;
+
+            /** ________________________ WIP ________________________*/
+            reference back ();
+
+            /** ________________________ WIP ________________________*/
+            const_reference back () const;
+
+            // Modifiers:
+
+            /*
+            ** Range
+            */
+            /** ________________________ WIP ________________________*/
+            template <class InputIterator>
+                void assign (InputIterator first, InputIterator last);
+            
+            /*
+            ** Fill
+            */
+            /** ________________________ WIP ________________________*/
+            void assign (size_type n, const value_type& val);
+
+            /** ________________________ WIP ________________________*/
+            void push_back (const value_type& val);
+
+            /** ________________________ WIP ________________________*/
+            void pop_back();
+
+            /*
+            ** Single element
+            */
+            /** ________________________ WIP ________________________*/
+            iterator insert (iterator position, const value_type& val);
+
+            /*
+            ** Fill
+            */
+            /** ________________________ WIP ________________________*/
+            void insert (iterator position, size_type n, const value_type& val);
+
+            /*
+            ** Range:
+            */
+            /** ________________________ WIP ________________________*/
+            template <class InputIterator>
+                void insert (iterator position, InputIterator first, InputIterator last);
+
+            /** ________________________ WIP ________________________*/
+            iterator erase (iterator position);
+
+            /** ________________________ WIP ________________________*/
+            iterator erase (iterator first, iterator last);
+
+            /** ________________________ WIP ________________________*/
+            void swap (vector& x);
+
+            /** ________________________ WIP ________________________*/
+            void clear();
     };
+
+    // Non-member function overloads
+    
+    /** ________________________ WIP ________________________*/
+    template <class T, class Alloc>
+        bool operator== (const vector<T, Alloc>& lhs, const vector<T, Alloc>& rhs);
+
+    /** ________________________ WIP ________________________*/
+    template <class T, class Alloc>
+        bool operator!= (const vector<T, Alloc>& lhs, const vector<T, Alloc>& rhs);
+    
+    /** ________________________ WIP ________________________*/
+    template <class T, class Alloc>
+        bool operator<  (const vector<T, Alloc>& lhs, const vector<T, Alloc>& rhs);
+
+    /** ________________________ WIP ________________________*/
+    template <class T, class Alloc>
+        bool operator<= (const vector<T, Alloc>& lhs, const vector<T, Alloc>& rhs);
+
+    /** ________________________ WIP ________________________*/
+    template <class T, class Alloc>
+        bool operator>  (const vector<T, Alloc>& lhs, const vector<T, Alloc>& rhs);
+
+    /** ________________________ WIP ________________________*/
+    template <class T, class Alloc>
+        bool operator>= (const vector<T, Alloc>& lhs, const vector<T, Alloc>& rhs);
 }
 // =============================================================================
 
